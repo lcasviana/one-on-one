2d1278bbd260de15b9a7c655b6a1d74974d78972
Lucas Viana <lcasviana@gmail.com>
2021-04-18 17:45:34 -0300

    Atualizando arquivo principal para arquitetura modular

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:oneonones/pages/home.page.dart';
-import 'package:oneonones/pages/landing.page.dart';
-import 'package:oneonones/pages/login.page.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:oneonones/app/app.module.dart';
+import 'package:oneonones/app/app.widget.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'One-on-one\'s',
-      initialRoute: '/landing',
-      routes: {
-        '/': (context) => HomePage(),
-        '/landing': (context) => LandingPage(),
-        '/login': (context) => LoginPage(),
-      },
-      theme: ThemeData(brightness: Brightness.light),
-      darkTheme: ThemeData(brightness: Brightness.dark),
-      themeMode: ThemeMode.dark,
-    );
-  }
-}
+void main() => runApp(ModularApp(module: AppModule(), child: App()));
